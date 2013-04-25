514cec7c3dde53510838775a6790637f1eb2181d
David Stolarsky <david.stolarsky@gmail.com>
2013-04-25 15:20:00 -0400

    better version publisher

--- ShareTags.sh
+++ ShareTags.sh
@@ -5,12 +5,14 @@ echo "var refs=[\n" > ~/Dropbox/Public/Venus/refs.js
 git for-each-ref --format="%(objectname)" refs/tags | \
 while read entry
 do
-        git show $entry:./venus.html > ~/Dropbox/Public/Venus/$entry.html
-	echo "  '$entry',\n" >> ~/Dropbox/Public/Venus/refs.js
+        mkdir -p ~/Dropbox/Public/Venus/$entry
+        git archive $entry | tar -x -C ~/Dropbox/Public/Venus/$entry
+        rm ~/Dropbox/Public/Venus/$entry/index.html
+
+	    echo "  '$entry',\n" >> ~/Dropbox/Public/Venus/refs.js
         #echo $ref
 done
 
 echo "];" >> ~/Dropbox/Public/Venus/refs.js
 
 cp index.html ~/Dropbox/Public/Venus/
-cp Box2d.min.js ~/Dropbox/Public/Venus/
