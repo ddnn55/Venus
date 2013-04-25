371d03503d0acb3e5abfa48fbdf5a4064aff8703
David Stolarsky <david.stolarsky@gmail.com>
2013-04-25 16:44:50 -0400

    instantaneous contract message

--- VenusBond.js
+++ VenusBond.js
@@ -1,34 +1,44 @@
-var   b2Vec2 = Box2D.Common.Math.b2Vec2
-   ,  b2AABB = Box2D.Collision.b2AABB
-	,	b2BodyDef = Box2D.Dynamics.b2BodyDef
-	,	b2Body = Box2D.Dynamics.b2Body
-	,	b2FixtureDef = Box2D.Dynamics.b2FixtureDef
-	,	b2Fixture = Box2D.Dynamics.b2Fixture
-	,	b2World = Box2D.Dynamics.b2World
-	,	b2MassData = Box2D.Collision.Shapes.b2MassData
-	,	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
-	,	b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
-	,	b2DebugDraw = Box2D.Dynamics.b2DebugDraw
-       ,	b2MouseJointDef =  Box2D.Dynamics.Joints.b2MouseJointDef
-       ,	b2DistanceJointDef =  Box2D.Dynamics.Joints.b2DistanceJointDef
-   ;
+var b2Vec2 = Box2D.Common.Math.b2Vec2,
+  b2AABB = Box2D.Collision.b2AABB,
+  b2BodyDef = Box2D.Dynamics.b2BodyDef,
+  b2Body = Box2D.Dynamics.b2Body,
+  b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
+  b2Fixture = Box2D.Dynamics.b2Fixture,
+  b2World = Box2D.Dynamics.b2World,
+  b2MassData = Box2D.Collision.Shapes.b2MassData,
+  b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+  b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+  b2DebugDraw = Box2D.Dynamics.b2DebugDraw,
+  b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef,
+  b2DistanceJointDef = Box2D.Dynamics.Joints.b2DistanceJointDef;
 
-function Bond(cellA, cellB) {
-   cellA.bonds.push(this);
-   cellB.bonds.push(this);
+function Bond(cellA, cellB, angle) {
+  cellA.bonds.push(this);
+  cellB.bonds.push(this);
 
-   var j = new b2DistanceJointDef();
-   j.collideConnected = true;
-   j.length = 2.0;
-   j.dampingRatio = 0.9;
-   j.frequencyHz = 2;
-   
-   j.bodyA = cellA.body;
-   j.bodyB = cellB.body;
+  this.cellA = cellA;
+  this.cellB = cellB;
+  this.angle = angle;
 
-   this.joint = world.CreateJoint(j);
+  var j = new b2DistanceJointDef();
+  j.collideConnected = true;
+  j.length = 2.0;
+  j.dampingRatio = 0.9;
+  j.frequencyHz = 2;
+
+  j.bodyA = cellA.body;
+  j.bodyB = cellB.body;
+
+  this.joint = world.CreateJoint(j);
 }
 
-Bond.prototype.Contract = function() {
-  this.joint.SetLength(1.0);
+Bond.prototype.Contract = function(sender) {
+  if(this.angle == 0.0) {
+    this.joint.SetLength(1.0);
+    if (sender === this.cellA)
+      this.cellB.Contract(this);
+    else
+      this.cellA.Contract(this);
+  }
+
 }
