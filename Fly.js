4f8a57a18a251f9311621d451305c5d01fd2981a
David Stolarsky <david.stolarsky@gmail.com>
2013-04-25 14:34:00 -0400

    local bite start

---
+++ Fly.js
@@ -0,0 +1,34 @@
+var   b2Vec2 = Box2D.Common.Math.b2Vec2
+   ,  b2AABB = Box2D.Collision.b2AABB
+	,	b2BodyDef = Box2D.Dynamics.b2BodyDef
+	,	b2Body = Box2D.Dynamics.b2Body
+	,	b2FixtureDef = Box2D.Dynamics.b2FixtureDef
+	,	b2Fixture = Box2D.Dynamics.b2Fixture
+	,	b2World = Box2D.Dynamics.b2World
+	,	b2MassData = Box2D.Collision.Shapes.b2MassData
+	,	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
+	,	b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
+	,	b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+       ,	b2MouseJointDef =  Box2D.Dynamics.Joints.b2MouseJointDef
+       ,	b2DistanceJointDef =  Box2D.Dynamics.Joints.b2DistanceJointDef
+   ;
+
+function Fly(x, y, radius) {
+   var fixDef = new b2FixtureDef;
+   fixDef.density = 1.0;
+   fixDef.friction = 0.5;
+   fixDef.restitution = 0.2;
+   fixDef.shape = new b2CircleShape(
+      radius
+   );
+
+   var bodyDef = new b2BodyDef;
+   bodyDef.type = b2Body.b2_dynamicBody;
+   bodyDef.position.x = x;
+   bodyDef.position.y = y;
+   
+   this.body = world.CreateBody(bodyDef);
+   this.body.CreateFixture(fixDef);
+   this.body.SetUserData(this);
+}
+
